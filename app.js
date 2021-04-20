2861627b87fc4e5eae2ffa95030989e01bcba611
Lantana Ali <lantanaali20@gmail.com>
2021-04-20 17:02:26 +0100

    Cant go further - think we need jquery

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 const Game = require('./src/game')
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.set('views','./views')
 app.set('view engine', 'ejs')
 
@@ -10,8 +13,8 @@ app.get('/', function(req,res) {
   res.render('pages/openingpage')})
 
 app.post('/gamepage', function(req,res) {
-    console.log(req.body)
-    res.render('pages/gamepage')})
+  let game = new Game(req.body.one, req.body.two);
+   res.render('pages/gamepage', {playerOne: req.body.one,  playerTwo: req.body.two, playerOneHealth: game.player1HP, playerTwoHealth: game.player2HP})})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- src/game.js
+++ src/game.js
@@ -4,8 +4,14 @@ class Game{
         this.player1 = player2;
         this.player1HP = 100;
         this.player2HP = 100;
-
     }
+    attackOne(){
+        this.player1HP -= 10;
+    }
+    attackTwo() {
+        this.player2HP -= 10;
+    }
+    
 }
 
 module.exports = Game
