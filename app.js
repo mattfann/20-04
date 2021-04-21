fe501fd1269dd46c24b928e07045710e4f340b34
Matthew Fanning <mfcareers@yahoo.com>
2021-04-21 15:24:03 +0100

    d

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Game = require('./src/game')
+let game = new Game()
+
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
@@ -13,8 +15,17 @@ app.get('/', function(req,res) {
   res.render('pages/openingpage')})
 
 app.post('/gamepage', function(req,res) {
-  let game = new Game(req.body.one, req.body.two);
+  game.p1 = req.body.one;
+  game.p2 = req.body.two;
    res.render('pages/gamepage', {playerOne: req.body.one,  playerTwo: req.body.two, playerOneHealth: game.player1HP, playerTwoHealth: game.player2HP})})
+
+app.post('/attack1', function(req,res) {
+    game.attackOne();
+     res.render('pages/attack1', {playerOne: game.p1,  playerTwo: game.p2, playerOneHealth: game.player1HP, playerTwoHealth: game.player2HP})})   
+
+app.post('/attack2', function(req,res) {
+    game.attackTwo();
+      res.render('pages/attack2', {playerOne: game.p1,  playerTwo: game.p2, playerOneHealth: game.player1HP, playerTwoHealth: game.player2HP})}) 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 class Game{
-    constructor(player1,player2){
-        this.player1 = player1;
-        this.player1 = player2;
+    constructor(){
+        this.p1 = 'player1 name';
+        this.p2 = 'player2 name';
         this.player1HP = 100;
         this.player2HP = 100;
     }
